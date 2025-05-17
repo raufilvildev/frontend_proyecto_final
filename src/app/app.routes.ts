060dd575c49ac84fd65baa22747db719e4740394
Raúl Filigrana Villalba <raufilvil@gmail.com>
2025-05-17 10:07:19 +0200

    Creo proyecto de Angular. Instalo Angular Material, Tailwind y DaisyUI. Creo componentes básicos y configuro fichero de rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { EmailConfirmationComponent } from './pages/signup/email-confirmation/email-confirmation.component';
+import { SignupFormComponent } from './pages/signup/signup-form/signup-form.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'signup', component: SignupComponent, children: [
+        { path: '', component: SignupFormComponent},
+        { path: 'email_confirmation', component: EmailConfirmationComponent}
+    ] },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard/:user_id', component: DashboardComponent },
+    { path: 'settings/:user_id', component: SettingsComponent },
+    { path: '**', redirectTo: '/home' }
+];
---
+++ src/app/pages/signup/signup-form/signup-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  imports: [],
+  templateUrl: './signup-form.component.html',
+  styleUrl: './signup-form.component.css'
+})
+export class SignupFormComponent {
+
+}
