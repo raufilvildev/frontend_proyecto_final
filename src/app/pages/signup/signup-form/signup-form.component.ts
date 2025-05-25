286e1bf7136c26fff754af3377b5f40fbd06f719
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-05-25 12:07:11 +0200

    fix date for mySQL, regsitarse btn amd gender text

--- src/app/pages/signup/signup-form/signup-form.component.ts
+++ src/app/pages/signup/signup-form/signup-form.component.ts
@@ -116,7 +116,7 @@ export class SignupFormComponent {
       name: `${formValue.firstName} ${formValue.lastName}`,
       email: formValue.email!,
       phone: formValue.phone!,
-      birth_date: new Date(formValue.birthDate!),
+      birth_date: this.formatDateMySQL(new Date(formValue.birthDate!)),
       gender: formValue.gender!,
       username: formValue.username!,
       password: formValue.password!,
@@ -131,5 +131,13 @@ export class SignupFormComponent {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  formatDateMySQL(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
   }
 }
