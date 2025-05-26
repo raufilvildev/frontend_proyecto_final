66bdf0bfa6d9b85bebc097dff1983cc11fd375f3
gtarrojo <175803244+gtarrojo@users.noreply.github.com>
2025-05-26 08:42:19 +0200

    add 404 error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,20 +9,35 @@ import { SettingsComponent } from './pages/settings/settings.component';
 import { ChangePasswordComponent } from './pages/change-password/change-password.component';
 import { ChangePasswordConfirmationComponent } from './pages/login/change-password-confirmation/change-password-confirmation.component';
 import { LoginFormComponent } from './pages/login/login-form/login-form.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: '/home' },
-    { path: 'home', component: HomeComponent },
-    { path: 'signup', component: SignupComponent, children: [
-        { path: '', component: SignupFormComponent},
-        { path: 'email_confirmation/:user_id', component: EmailConfirmationComponent}
-    ] },
-    { path: 'login', component: LoginComponent , children: [
-        { path: '', component: LoginFormComponent },
-        { path: 'change_password_confirmation/:user_id', component: ChangePasswordConfirmationComponent },
-        { path: 'change_password/:user_id', component: ChangePasswordComponent }
-    ]},
-    { path: 'dashboard/:user_id', component: DashboardComponent },
-    { path: 'settings/:user_id', component: SettingsComponent },
-    { path: '**', redirectTo: '/home' }
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    children: [
+      { path: '', component: SignupFormComponent },
+      {
+        path: 'email_confirmation/:user_id',
+        component: EmailConfirmationComponent,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    children: [
+      { path: '', component: LoginFormComponent },
+      {
+        path: 'change_password_confirmation/:user_id',
+        component: ChangePasswordConfirmationComponent,
+      },
+      { path: 'change_password/:user_id', component: ChangePasswordComponent },
+    ],
+  },
+  { path: 'dashboard/:user_id', component: DashboardComponent },
+  { path: 'settings/:user_id', component: SettingsComponent },
+  { path: '**', component: Error404Component },
 ];
