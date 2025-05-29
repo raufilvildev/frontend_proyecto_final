dec3cb663cdee812be32ac1b3440c6a7c66c2548
DruMoDev <dev.morera@gmail.com>
2025-05-29 22:23:02 +0200

    Created layout with header, footer and main

---
+++ src/app/shared/components/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-header',
+    imports: [CommonModule],
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+    appName = 'Task Manager';
+}
