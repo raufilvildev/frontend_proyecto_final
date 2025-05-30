d16b81257f815621376a3ea8ef5dda59b0c4b49b
Lucho <lsepu19@gmail.com>
2025-05-30 21:39:04 +0200

    Secciones del dashboard
    Delimitaciones del dashboard con angular-split

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DashboardSidebarComponent } from "./dashboard-sidebar/dashboard-sidebar.component";
+import { DashboardMainComponent } from "./dashboard-main/dashboard-main.component";
+import { DashboardTaskDetailsComponent } from "./dashboard-task-details/dashboard-task-details.component";
+import { AngularSplitModule } from 'angular-split';
+
 
 @Component({
-    selector: 'app-dashboard',
-    imports: [],
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.css',
+  selector: 'app-dashboard',
+  imports: [DashboardSidebarComponent, DashboardMainComponent, DashboardTaskDetailsComponent,AngularSplitModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {}
