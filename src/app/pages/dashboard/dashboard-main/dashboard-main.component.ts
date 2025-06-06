99e2496c5a27c8922a97acce5001b64896c0a0ee
Lucho <lsepu19@gmail.com>
2025-06-06 08:35:14 +0200

    Componente para tareas + signals de dashboard
    Creo un componente para poder reutilziar las tareas luego, además instalo angular material y me pongo con la lógica de los componentes añadiendo sigma. En prueba de check con angular materials

--- src/app/pages/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/pages/dashboard/dashboard-main/dashboard-main.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { TaskItemComponent } from "../../../shared/components/task/task-item/task-item.component";
 
 @Component({
   selector: 'app-dashboard-main',
-  imports: [],
+  imports: [ TaskItemComponent],
   templateUrl: './dashboard-main.component.html',
   styleUrl: './dashboard-main.component.css'
 })
 export class DashboardMainComponent {
+  @Input() tasks: any[] = [];
+  @Input() selectedTask: any = null;
+  @Input() selectedSubtask: any = null;
+  @Output() taskSelected = new EventEmitter<any>();
+  @Output() subtaskSelected = new EventEmitter<{subtask: any, parentTask: any}>();
 
+  selectTask(task: any) {
+    this.taskSelected.emit(task);
+  }
+
+  selectSubtask(subtask: any, parentTask: any) {
+    this.subtaskSelected.emit({subtask, parentTask});
+  }
 }
--- src/app/pages/dashboard/dashboard-task-details/dashboard-task-details.component.ts
+++ src/app/pages/dashboard/dashboard-task-details/dashboard-task-details.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-task-details',
+  standalone: true,
   imports: [],
   templateUrl: './dashboard-task-details.component.html',
   styleUrl: './dashboard-task-details.component.css'
 })
 export class DashboardTaskDetailsComponent {
-
+  @Input() task: any = null;
+  @Input() subtask: any = null;
 }
